3af7aa57cb1747e36f3e1e0e69a369bbc2dc6fbe
Rashid Ali <rashidkalwar1456@gmail.com>
2022-11-23 09:52:51 +0500

    add loader

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
+const repo = "portfolio";
+const assetPrefix = `/${repo}/`;
+const basePath = `/${repo}`;
 const nextConfig = {
   reactStrictMode: true,
-}
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+  images: {
+    loader: "akamai",
+    path: "",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
