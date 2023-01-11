beba6fa24970ed8879ee8f7753ffb5d92f4d50fa
rashidkalwar <80094039+rashidkalwar@users.noreply.github.com>
2023-01-11 08:31:24 +0000

    Deploying to gh-pages from @ rashidkalwar/rashidkalwar.github.io@710402f930f40b4c0d5a88e11c1b8c2ebc3ce21f 🚀

--- _next/static/GwVFwZa3ata_9XBFlyQC7/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/ef6529d7-b189f0c95d34b364.js","static/chunks/427-d6e7cf87c58143ef.js","static/chunks/pages/index-aab4fef7939b696c.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
